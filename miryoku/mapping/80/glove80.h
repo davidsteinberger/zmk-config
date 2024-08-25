73608f39a9f76f6c1b97ea78fd3baad8cd713c40
David Steinberger <mail.steinberger@gmail.com>
2024-08-25 20:34:18 +0200

    add glove80

--- miryoku/mapping/80/glove80.h
+++ miryoku/mapping/80/glove80.h
@@ -11,12 +11,12 @@
      K20, K21, K22, K23, K24,                                    K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                                    K35, K36, K37, N38, N39 \
 ) \
-XXX  XXX  XXX  XXX  XXX                                               XXX  XXX  XXX  XXX  XXX \
-XXX  XXX  XXX  XXX  XXX  XXX                                     XXX  XXX  XXX  XXX  XXX  XXX \
-XXX  K00  K01  K02  K03  K04                                     K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14                                     K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24  XXX  XXX  XXX       XXX  XXX  XXX  K25  K26  K27  K28  K29  XXX \
-XXX  XXX  XXX  XXX  XXX       K32  K33  K34       K35  K36  K37       XXX  XXX  XXX  XXX  XXX
+&kp F1  &kp F2  &kp F3  &kp F4  &kp F5                                               &kp F6  &kp F7  &kp F8  &kp F9  &kp F10 \
+&kp GRAVE  &kp N1  &kp N2  &kp N3  &kp N4  &kp N5                                     &kp N6  &kp N7  &kp N8  &kp N9  &kp N0  &kp BSPC \
+&kp TAB  K00  K01  K02  K03  K04                                     K05  K06  K07  K08  K09  &kp BSLH \
+&kp ESC  K10  K11  K12  K13  K14                                     K15  K16  K17  K18  K19  &kp RET \
+&kp LSHFT  K20  K21  K22  K23  K24  &kp LSHFT  &kp LGUI  &kp LCTRL       &kp RCTRL  &kp RGUI  &kp RSHFT  K25  K26  K27  K28  K29  &kp RSHFT \
+&kp UP  &kp DOWN  &kp LT  &kp LPAR  &kp LBKT       K33  K34  K32       K37  K35  K36       &kp RBKT  &kp RPAR  &kp GT  &kp LEFT  &kp RIGHT
 
 #endif
 
