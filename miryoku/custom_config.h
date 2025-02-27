9c720340b6b86ce720f2b48ff5335b8142174c4f
David Steinberger <mail.steinberger@gmail.com>
2025-02-27 21:27:34 +0100

    add adaptive key

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,10 +4,11 @@
 #define MIRYOKU_NAV_VI
 #define MIRYOKU_CLIPBOARD_MAC
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
-// #define MIRYOKU_KLUDGE_TAPDELAY
+//#define MIRYOKU_KLUDGE_TAPDELAY
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
+MIRYOKU_X(ALT,    "Alt") \
 MIRYOKU_X(EXTRA,  "Extra") \
 MIRYOKU_X(TAP,    "Tap") \
 MIRYOKU_X(BUTTON, "Button") \
@@ -16,27 +17,24 @@ MIRYOKU_X(MOUSE,  "Mouse") \
 MIRYOKU_X(MEDIA,  "Media") \
 MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
-MIRYOKU_X(FUN,    "Fun") \
-MIRYOKU_X(SEN,    "Sen")
+MIRYOKU_X(FUN,    "Fun")
 
 #define U_BASE   0
-#define U_EXTRA  1
-#define U_TAP    2
-#define U_BUTTON 3
-#define U_NAV    4
-#define U_MOUSE  5
-#define U_MEDIA  6
-#define U_NUM    7
-#define U_SYM    8
-#define U_FUN    9
-#define U_SEN    10
+#define U_ALT    1
+#define U_EXTRA  2
+#define U_TAP    3
+#define U_BUTTON 4
+#define U_NAV    5
+#define U_MOUSE  6
+#define U_MEDIA  7
+#define U_NUM    8
+#define U_SYM    9
+#define U_FUN    10
 
-#define MIRYOKU_LAYERMAPPING_SEN MIRYOKU_MAPPING
+#define MIRYOKU_LAYERMAPPING_ALT MIRYOKU_MAPPING
 
 #define AS(keycode) &as keycode
-#define SC_SPACE_SHIFT &sc_mo_space_shift U_NAV 0
 #define RALT_MINUS_DQT &ht_minus_dqt RALT 0
-#define SC_DOT &sc_dot 0 0
 
 #define MIRYOKU_LAYER_MOUSE \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,  \
@@ -44,15 +42,21 @@ U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA
 U_NA,              &kp RALT,          &u_to_U_SYM,       &u_to_U_MOUSE,     U_NA,              U_WH_L,            U_WH_D,            U_WH_U,            U_WH_R,            U_NU,   \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
 
+#define MIRYOKU_LAYER_NAV \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      &u_to_U_ALT,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
+U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           &kp INS,           \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+
 #define MIRYOKU_LAYER_BASE \
 &kp B,             &kp L,             &kp D,             &kp W,             &kp Z,             &apos_under,             &kp F,             &kp O,             &kp U,             &kp J,     \
 U_MT(LGUI, N),     U_MT(LALT, R),     U_MT(LCTL, T),    U_MT(LSHFT, S),    &kp G,             &kp Y,             U_MT(LSHFT, H),    U_MT(LCTL, A),    U_MT(LALT, E),     U_MT(LGUI, I), \
-U_LT(U_BUTTON, Q), U_MT(RALT, X),     &kp M,             &kp C,             &kp V,             &kp K,             &kp P,             SC_DOT,         RALT_MINUS_DQT,   &slash_lt ,   \
+U_LT(U_BUTTON, Q), U_MT(RALT, X),     &kp M,             &kp C,             &kp V,             &kp K,             &kp P,             &dot_gt,         RALT_MINUS_DQT,   &slash_lt ,   \
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
-#define MIRYOKU_LAYER_SEN \
-&trans,            &trans,            &trans,            &trans,         &trans,            &trans,            &trans,            &trans,            &trans,            &trans, \
-&trans,            &trans,            &trans,            &trans,         &trans,            &trans,            &trans,            &trans,            &trans,            &trans, \
-&trans,            &trans,            &trans,            &trans,         &trans,            &trans,            &trans,            &trans,            &trans,            &trans, \
-U_NP,              U_NP,              U_NP,              SC_SPACE_SHIFT, &trans,            &trans,            &trans,            U_NP,              U_NP,              U_NP
+#define MIRYOKU_LAYER_ALT \
+&kp B,             &kp L,             &kp D,             &kp W,             &kp Z,             &apos_under,             &kp F,             &kp O,             &kp U,             &kp J,     \
+U_MT(LGUI, N),     U_MT(LALT, R),     U_MT(LCTL, T),    U_MT(LSHFT, S),    &kp G,             &kp Y,             U_MT(LSHFT, H),    U_MT(LCTL, A),    U_MT(LALT, E),     U_MT(LGUI, I), \
+U_LT(U_BUTTON, Q), U_MT(RALT, X),     &kp M,             &kp C,             &kp V,             &kp K,             &kp P,             &dot_gt,         RALT_MINUS_DQT,   &slash_lt ,   \
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),&ak_sc,&ak_tab,U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
