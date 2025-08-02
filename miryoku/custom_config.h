787e310299e8a4fe845777cb06b27aedce168340
David Steinberger <mail.steinberger@gmail.com>
2025-08-02 11:19:54 +0200

    properly handle cradio

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,6 +4,7 @@
 #define MIRYOKU_NAV_VI
 #define MIRYOKU_CLIPBOARD_MAC
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
+#define MIRYOKU_KLUDGE_DOUBLETAPBOOT
 //#define MIRYOKU_KLUDGE_TAPDELAY
 
 #define MIRYOKU_LAYER_LIST \
@@ -60,3 +61,8 @@ U_MT(LGUI, N),     U_MT(LALT, R),     U_MT(LCTL, T),    U_MT(LSHFT, S),    &kp G
 U_LT(U_BUTTON, Q), U_MT(RALT, X),     &kp M,             &kp C,             &kp V,             &kp K,             &kp P,             &dot_gt,         RALT_MINUS_DQT,   &slash_lt ,   \
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),&ak_sc,&ak_tab,U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
+#define MIRYOKU_LAYER_NUM \
+&kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp SEMI,          &kp N4,            &kp N5,            &kp N6,            &kp EQUAL,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp GRAVE,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,          U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          &kp DOT,              \
+U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
