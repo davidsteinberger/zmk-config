796a1c725af1f77a2c67fad0b6ef1b9e251ec0a5
David Steinberger <mail.steinberger@gmail.com>
2025-11-17 08:08:13 +0100

    sticky shift

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -43,13 +43,13 @@ MIRYOKU_X(FUN,    "Fun")
 #define U_MTR(mod, keycode) &hmr mod keycode
 
 #define MIRYOKU_LAYER_MOUSE \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,  \
+U_NP,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,  \
 &kp LGUI,          &kp LALT,          &kp LCTL,         &kp LSHFT,         U_NA,              U_MS_L,            U_MS_D,            U_MS_U,            U_MS_R,            &kp ESC, \
 U_NA,              &kp RALT,          &u_to_U_SYM,       &u_to_U_MOUSE,     U_NA,              U_WH_L,            U_WH_D,            U_WH_U,            U_WH_R,            U_NU,   \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NAV \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      &u_to_U_ALT,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+U_NP,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      &u_to_U_ALT,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
 U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           &kp INS,           \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
@@ -67,7 +67,13 @@ U_LT(U_BUTTON, Q), U_MTL(RALT, X),     &kp M,             &kp C,             &kp
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),&ak_sc,&ak_tab,U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NUM \
-&kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_NP,            \
 &kp SEMI,          &kp N4,            &kp N5,            &kp N6,            &kp EQUAL,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp GRAVE,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,          &kp COMMA,         &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          &kp DOT,              \
 U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+#define MIRYOKU_LAYER_SYM \
+&kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_NP,            \
+&kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
--- miryoku/mapping/80/glove80.h
+++ miryoku/mapping/80/glove80.h
@@ -6,6 +6,7 @@
 #define XXX &none
 
 #define AS(keycode) &as keycode
+#define SK_SHIFT &kp_sticky LSHFT LSHFT
 
 #define MIRYOKU_LAYOUTMAPPING_GLOVE80( \
      K00, K01, K02, K03, K04,                                    K05, K06, K07, K08, K09, \
@@ -17,7 +18,7 @@
 &kp GRAVE  &kp N1  &kp N2  &kp N3  &kp N4  &kp N5                                     &kp N6  &kp N7  &kp N8  &kp N9  &kp N0  &kp BSPC \
 &kp TAB  K00  K01  K02  K03  K04                                     K05  K06  K07  K08  K09  AS(SEMI) \
 &kp ESC  K10  K11  K12  K13  K14                                     K15  K16  K17  K18  K19  &comma_qmark \
-&kp LSHFT  K20  K21  K22  K23  K24  &ctrl_alt  &kp LGUI  &kp LALT   &kp RALT  &kp RGUI  &tmux  K25  K26  K27  K28  K29  &kp RSHFT \
+SK_SHIFT K20  K21  K22  K23  K24  &ctrl_alt  &kp LGUI  &kp LALT   &kp RALT  &kp RGUI  &kp RCTRL  K25  K26  K27  K28  K29  SK_SHIFT \
 &kp UP  &kp DOWN  &kp LT  &kp LPAR  AS(LBKT)       K33  K34  K32       K37  K35  K36       AS(RBKT)  &kp RPAR  &kp GT  &kp LEFT  &kp RIGHT
 
 #endif
