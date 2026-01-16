3381eeca438c0024f0da8f7c93eb83b04d5a31ce
David Steinberger <mail.steinberger@gmail.com>
2026-01-16 16:52:48 +0100

    minor improvements

--- config/miryoku/custom_config.h
+++ config/miryoku/custom_config.h
@@ -56,24 +56,25 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 
 #define MIRYOKU_LAYER_BASE \
 &kp B,             &kp L,             &kp D,             &kp W,             &kp Z,             &apos_under,             &kp F,             &kp O,             &kp U,             &kp J,     \
-U_MTL(LGUI, N),     U_MTL(LALT, R),     U_MTL(LCTL, T),    U_MTL(LSHFT,S),    &kp G,             &kp Y,             &hmr LSHFT H,    U_MTR(LCTL, A),    U_MTR(LALT, E),     U_MTR(LGUI, I), \
+U_MTL(LGUI, N),     U_MTL(LALT, R),     U_MTL(LCTL, T),    U_MTL(LSHFT,S),    U_MTL(LGUI, G),             U_MTR(LGUI, Y),             &hmr LSHFT H,    U_MTR(LCTL, A),    U_MTR(LALT, E),     U_MTR(LGUI, I), \
 U_LT(U_BUTTON, Q), U_MTL(RALT, X),     &kp M,             &kp C,             &kp V,             &kp K,             &kp P,             &dot_gt,         RALT_MINUS_DQT,   &slash_lt ,   \
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_ALT \
 &kp B,             &kp L,             &kp D,             &kp W,             &kp Z,             &apos_under,             &kp F,             &kp O,             &kp U,             &kp J,     \
-U_MTL(LGUI, N),     U_MTL(LALT, R),     U_MTL(LCTL, T),    U_MTL(LSHFT, S),    &kp G,             &kp Y,             U_MTR(LSHFT, H),    U_MTR(LCTL, A),    U_MTR(LALT, E),     U_MTR(LGUI, I), \
+U_MTL(LGUI, N),     U_MTL(LALT, R),     U_MTL(LCTL, T),    U_MTL(LSHFT,S),    U_MTL(LGUI, G),             U_MTR(LGUI, Y),             &hmr LSHFT H,    U_MTR(LCTL, A),    U_MTR(LALT, E),     U_MTR(LGUI, I), \
 U_LT(U_BUTTON, Q), U_MTL(RALT, X),     &kp M,             &kp C,             &kp V,             &kp K,             &kp P,             &dot_gt,         RALT_MINUS_DQT,   &slash_lt ,   \
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),&ak_sc,&ak_tab,U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NUM \
 &kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_NP,            \
-&kp SEMI,          &kp N4,            &kp N5,            &kp N6,            &kp EQUAL,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp SEMI,          &kp N4,            &kp N5,            &kp N6,            &kp EQUAL,         &kp LGUI,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp GRAVE,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,          &kp COMMA,         &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          &kp DOT,              \
 U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
 &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_NP,            \
-&kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          &kp LGUI,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
--- config/miryoku/mapping/80/glove80.h
+++ config/miryoku/mapping/80/glove80.h
@@ -15,11 +15,11 @@
      N30, N31, K32, K33, K34,                                    K35, K36, K37, N38, N39 \
 ) \
 &kp F1  &kp F2  &kp F3  &kp F4  &kp F5                                               &kp F6  &kp F7  &kp F8  &kp F9  &kp F10 \
-&kp GRAVE  &kp N1  &kp N2  &kp N3  &kp N4  &kp N5                                     &kp N6  &kp N7  &kp N8  &kp N9  &kp N0  &kp BSPC \
-AS(LBKT)  K00  K01  K02  K03  K04                                     K05  K06  K07  K08  K09  AS(RBKT)\
-&s_par_esc  K10  K11  K12  K13  K14                                     K15  K16  K17  K18  K19  &comma_qmark \
+U_PST  &kp N1  &kp N2  &kp N3  &kp N4  &kp N5                                     &kp N6  &kp N7  &kp N8  &kp N9  &kp N0  &kp BSPC \
+U_CPY  K00  K01  K02  K03  K04                                     K05  K06  K07  K08  K09  &kp EQUAL\
+&kp ESC  K10  K11  K12  K13  K14                                     K15  K16  K17  K18  K19  &comma_qmark \
 &kp LSHFT K20  K21  K22  K23  K24  &ctrl_alt  &kp LGUI  &kp LALT   &kp RALT  &kp RGUI  &kp RCTRL  K25  K26  K27  K28  K29  &kp RSHFT \
-U_CPY U_PST &kp LBRC  &kp LBKT  &kp LPAR       K33  K34  K32       K37  K35  K36       &kp RPAR      &kp RBKT  &kp RBRC  &kp LEFT  &kp RIGHT
+&kp UP  &kp DOWN &kp LBRC  &kp LBKT  &kp LPAR       K33  K34  K32       K37  K35  K36       &kp RPAR      &kp RBKT  &kp RBRC  &kp LEFT  &kp RIGHT
 
 #endif
 
